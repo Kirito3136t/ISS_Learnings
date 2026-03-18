7974ac2770ca56bbd6752afe6e6ebeebcb0ea6fb
Kirito3136t <2020.akash.choughule@ves.ac.in>
2026-03-18 15:06:30 +0530

    adding hashmap with streams

--- lambda_and_streams/lambda_stream.java
+++ lambda_and_streams/lambda_stream.java
@@ -34,6 +34,35 @@ List<Integer> nums = List.of(2,6,4,3,7,9);
 int sum = nums.stream()
             .reduce(0,(acc,curr)->acc+curr);
 
+// custom comparator
+List<String> names = List.of("akash", "dev","akash", "rahul","karan","simran");
+names.stream()
+    .distinct()
+    .sorted((a,b)->b.compareTo(a))
+    .forEach(x->System.out.println(x))
+
+//flat map (used for nested list)
+List<List<String>> list = List.of(
+    List.of("Akash", "Rahul"),
+    List.of("Dev", "Shubham")
+);
+list.stream()
+    .flatmap(x->x.stream())
+    .forEach(x->System.out.println(x));
+
+// hashmap with streams
+Map<Integer, String> map = new HashMap<>();
+map.put(1, "Akash");
+map.put(2, "Dev");
+map.put(3, "Rahul");
+
+map.entrySet().stream()
+    .forEach(entry -> 
+        System.out.println(entry.getKey() + " " + entry.getValue())
+    );
+
+// we can use map.keys() or map.values() to parse acording to specific requirement
+
 
 
 
